1f824043c55323c63439f887a677f7d7da6c99a2
Kyoya_ecc <2210392@ecc.ac.jp>
2023-09-01 13:43:08 +0900

    Revert "修正"
    This reverts commit 3954990d459cc5b881c4cf8ab28fa12e6e5df418.

--- Product.client.tsx
+++ Product.client.tsx
@@ -31,24 +31,33 @@ export default function Product() {
     const filteredProducts = showOnlyDiscounted
         ? products.filter(product => product.discounts.length > 0)
         : products;
-
-    if (selectedProduct) {
-       return (
-        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8 bg-gradient-to-r from-blue-400 via-teal-500 to-green-400">
+    //ボタンのクリック部分
+   if (selectedProduct) {
+    return (
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8 flex flex-col items-center bg-gradient-to-r from-blue-400 via-teal-500 to-green-400">
             <button
                 onClick={resetSelectedProduct}
-                className="mb-4 bg-indigo-600 text-white px-4 py-2 rounded hover:bg-indigo-500"
+                className="self-start mb-4 bg-indigo-600 text-white px-6 py-2 rounded-lg hover:bg-indigo-500 transition-transform transform hover:scale-105"
             >
                 戻る
             </button>
-            <h2 className="text-2xl mb-4 text-gray-800">{selectedProduct.pname}</h2>
-            <img className="mb-4 w-full object-contain" src={`https://sabakan-backet.s3.ap-southeast-2.amazonaws.com/test/${selectedProduct.image}`} alt={selectedProduct.pname} />
-            <p className="mb-2 text-xl font-semibold text-gray-700">￥: {selectedProduct.price}円</p>
-            <p className="mb-2 text-lg text-gray-700">産地: {selectedProduct.production_area}</p>
-            <p className="mb-2 text-lg text-gray-700">容量: {selectedProduct.volume}</p>
+            <div className="w-full md:w-2/3 lg:w-1/2 bg-white p-6 rounded-lg shadow-lg">
+                <h2 className="text-4xl mb-6 text-black font-bold">{selectedProduct.pname}</h2>
+                <img className="mb-6 w-full h-64 object-cover rounded shadow" src={`https://sabakan-backet.s3.ap-southeast-2.amazonaws.com/test/${selectedProduct.image}`} alt={selectedProduct.pname} />
+                <p className="mb-4 text-2xl font-semibold text-gray-800">￥: {selectedProduct.price}円</p>
+                <p className="mb-4 text-lg text-gray-600">産地: {selectedProduct.production_area}</p>
+                <p className="mb-4 text-lg text-gray-600">容量: {selectedProduct.volume}</p>
+                <p className="mb-4 text-lg text-gray-600">状態: {selectedProduct.discounts[0]?.state?.stname || '不明'}</p>
+                <div className="mb-4 text-lg text-gray-600 flex items-center">
+                    <span className="mr-2">定価:</span>
+                    <span className="line-through mr-4">{selectedProduct.originalPrice}円</span>
+                    <span className="font-semibold">→ 値引き後： {selectedProduct.discountedPrice}円</span>
+                </div>
+                <p className="text-red-600 mr-2">値引き額: {selectedProduct.discountAmount}円</p>
+            </div>
         </div>
-      );
-    }
+    );
+   }
 
     return (
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8 bg-gradient-to-r from-blue-400 via-teal-500 to-green-400">
@@ -69,8 +78,7 @@ export default function Product() {
                         <div className="flex-1 p-4">
                             <h3 className="font-semibold text-gray-900 text-lg">{product.pname}</h3>
                             <p className="text-gray-600">￥: {product.price}円</p>
-                            <p className="text-gray-500">産地: {product.production_area}</p>
-                            <p className="text-gray-400">容量: {product.volume}</p>
+                            <p className="text-gray-400">状態: {product.discounts[0]?.state?.stname || '不明'}</p>
                         </div>
                         <div className="p-4 bg-gray-100 border-t border-gray-200">
                             <button onClick={() => displayDetails(product)} className="text-lg font-bold text-purple-600 hover:text-purple-700">
